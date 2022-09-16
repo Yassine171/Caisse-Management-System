1107dd5dc4563c2a1620518ffafdae8ce85ca2bb
Yassine <Oussiyassine1@gmail.com>
2022-09-16 01:12:02 +0100

    first commit

--- Backend/DB/tables.sql
+++ Backend/DB/tables.sql
@@ -1,44 +1,141 @@
-create table user(
-    id int primary key AUTO_INCREMENT,
-    name varchar(250),
-    email varchar(50),
-    password varchar(250),
-    role varchar(20),
-    status varchar(20),
-    UNIQUE (email)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 16 sep. 2022 à 02:06
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
 
-insert into
-    user(name, email, password, status, role)
-values
-    (
-        'Admin',
-        'admin@admin.com',
-        'admin',
-        'true',
-        'admin'
-    );
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- create table category(
---     id int NOT NULL AUTO_INCREMENT,
---     name varchar(255) NOT NULL UNIQUE,
---     primary key(id)
--- );
 
-create table transaction(
-    id int NOT NULL AUTO_INCREMENT,
-    libelle varchar(255) NOT NULL ,
-    date_transaction Date NOT NULL,
-    recette int,
-    depence int,
-    userID int NOT NULL,
-    primary key(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table bill(
-    id int NOT NULL AUTO_INCREMENT,
-    uuid varchar(200) NOT NULL UNIQUE,
-    date datetime NOT NULL,
-    transactionsDetails JSON DEFAULT NULL,
-    primary key(id)
-);
+--
+-- Base de données : `transaction`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `solde` int(11) NOT NULL,
+  `transactionsDetails` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `bill`
+--
+
+INSERT INTO `bill` (`id`, `date`, `solde`, `transactionsDetails`) VALUES
+(81, '2022-10-02 00:00:00', 52, '[{\"id\":77,\"libelle\":\"updated\",\"date_transaction\":\"2022-10-02T00:00:00.000Z\",\"recette\":100,\"depense\":50,\"userID\":1,\"solde\":\"50\"},{\"id\":78,\"libelle\":\"jkhk\",\"date_transaction\":\"2022-10-02T00:00:00.000Z\",\"recette\":69,\"depense\":67,\"userID\":1,\"solde\":\"52\"}]'),
+(88, '2022-10-01 00:00:00', 10, '[{\"id\":79,\"libelle\":\"iy\",\"date_transaction\":\"2022-10-01T00:00:00.000Z\",\"recette\":12,\"depense\":2,\"userID\":1,\"solde\":\"10\"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_transaction` date NOT NULL,
+  `recette` int(11) DEFAULT NULL,
+  `depense` int(11) DEFAULT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `libelle`, `date_transaction`, `recette`, `depense`, `userID`) VALUES
+(77, 'updated', '2022-10-02', 100, 50, 1),
+(78, 'jkhk', '2022-10-02', 69, 67, 1),
+(79, 'iy', '2022-10-01', 12, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `status`) VALUES
+(1, 'Admin', 'admin@admin.com', 'admin123', 'admin', 'true'),
+(2, 'yassine', 'yassine@yas.com', 'yassine', 'user', 'true');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_index` (`date`);
+
+--
+-- Index pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+
+--
+-- AUTO_INCREMENT pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
